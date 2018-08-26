731fe9af4416ed02557d7d180a83321598071b79
satyaP <sats.pasupuleti@gmail.com>
2018-08-26 16:40:36 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+    describe('Math',function() {
+        it('should return 9',function(){
+            assert.equal(9,3*3);
+
+        });
+        it('should return -8',function(){
+            assert.equal(-8,(3-4)*8);
+
+        });
+          
+
+    });
+  });
+});
